375871483cb6c00360e828d0b2a4940486e62bef
Sylvati <LatiAuthor@gmail.com>
2025-08-10 01:58:01 -0400

    add pixel clicking

--- index.js
+++ index.js
@@ -1,17 +1,147 @@
+// Alpha constants
+OPAQUE = 255;
+TRANSLUCENT = 128;
+TRANSPARENT = 0;
+
+// Fields
+let pixel_amount_element;
+
+// Currency
+let pixels = 0;
+
 // Wait till page loaded
 document.addEventListener("DOMContentLoaded", () => {
+  // Initialize fields
+  initialize_fields();
+
   // Load sprites into the holder
   load_into_holder();
 
-  // Make the pixels in the canvas
-  add_pixels_to_canvas();
-
   // Color the pixels in the canvas
-  color_pixels_in_canvas(1);
+  color_pixels_in_canvas(1, 0.5);
+
+  // Add click listener
+  intialize_canvas_click_listener();
 });
 
+// Function to initialize fields
+function initialize_fields() {
+  // Initialize the pixel amount header
+  pixel_amount_element = document.getElementById("pixel-amount");
+  update_pixels();
+}
+
+// Function holding all events that happen when the canvas is clicked.
+function on_canvas_clicked(ctx, x, y) {
+  // Get image data
+  const image_data = ctx.getImageData(
+    0,
+    0,
+    ctx.canvas.width,
+    ctx.canvas.height
+  );
+
+  // Get the location (y * width + x) * 4, because rgba for each pixel, y is skipping rows.
+  const index = (y * image_data.width + x) * 4;
+
+  // Get the color and alpha values
+  const r = image_data.data[index + 0];
+  const g = image_data.data[index + 1];
+  const b = image_data.data[index + 2];
+  const a = image_data.data[index + 3];
+
+  // Log the click
+  // console.log(`Clicked at canvas pixel: (${x}, ${y})`);
+
+  // If clickable,
+  if (is_clickable(a)) {
+    // Set the alpha
+    set_pixel_alpha(image_data, index, OPAQUE);
+
+    // Confirm changes
+    confirm_changes(ctx, image_data, 0, 0);
+
+    // Add a pixel
+    add_pixels(1);
+  }
+}
+
+// Function to ensure pixels text is updated
+function update_pixels() {
+  pixel_amount_element.textContent = `Pixels: ${pixels}`;
+}
+
+// Function to add pixels
+function add_pixels(amount) {
+  pixels += amount;
+  update_pixels();
+}
+
+// Function to subtract pixels
+function subtract_pixels(amount) {
+  pixels -= amount;
+  update_pixels();
+}
+
+// Function to check for clickability
+function is_clickable(alpha) {
+  return alpha > 0 && alpha < 255;
+}
+
+// Function to confirm image_data changes
+function confirm_changes(ctx, image_data, dx, dy) {
+  ctx.putImageData(image_data, dx, dy);
+}
+
+// Function to change rgba
+function setPixelRGBA(image_data, index, red, green, blue, alpha) {
+  setPixelRed(image_data, index, red);
+  setPixelBlue(image_data, index, blue);
+  setPixelGreen(image_data, index, green);
+  set_pixel_alpha(image_data, index, alpha);
+}
+
+// Function to change pixel r
+function setPixelRed(image_data, index, red) {
+  image_data.data[index + 0] = red;
+}
+
+// Function to change pixel g
+function setPixelGreen(image_data, index, green) {
+  image_data.data[index + 1] = green;
+}
+
+// Function to change pixel b
+function setPixelBlue(image_data, index, blue) {
+  image_data.data[index + 2] = blue;
+}
+
+// Function to change pixel alpha
+function set_pixel_alpha(image_data, index, alpha) {
+  image_data.data[index + 3] = alpha;
+}
+
+// Function to initialize the canvas listener
+function intialize_canvas_click_listener() {
+  // Find the canvas
+  const canvas = document.getElementById("painting-grid");
+  // Get the context
+  const ctx = canvas.getContext("2d");
+  // Add the listener
+  canvas.addEventListener("click", (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+
+    const x = Math.floor((e.clientX - rect.left) * scaleX);
+    const y = Math.floor((e.clientY - rect.top) * scaleY);
+
+    on_canvas_clicked(ctx, x, y);
+  });
+}
+
 // Function to color the pixels in the canvas
-function color_pixels_in_canvas(poke_num) {
+function color_pixels_in_canvas(poke_num, alpha_value) {
   // Just load the first for now.
   const img = new Image();
   img.src = `sprites/%23${poke_num}.png`;
@@ -19,63 +149,20 @@ function color_pixels_in_canvas(poke_num) {
   // When the image finishes loading
   img.onload = () => {
     // Create a fake canvas to use
-    const fake_canvas = document.createElement("canvas");
-    // Make the canvas the right size
-    fake_canvas.width = img.width;
-    fake_canvas.height = img.height;
+    const canvas = document.getElementById("painting-grid");
     // Get the canvas' context
-    const fake_canvas_2d = fake_canvas.getContext("2d");
+    const ctx = canvas.getContext("2d");
+
+    // Make the canvas the right size
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Set the global alpha value
+    ctx.globalAlpha = alpha_value;
+
     // Draw the image
-    fake_canvas_2d.drawImage(img, 0, 0);
-    // Get the pixel data
-    const img_data = fake_canvas_2d.getImageData(0, 0, img.width, img.height);
-    // Turn it into raw data
-    const raw_data = img_data.data;
-    console.log(raw_data);
-
-    // Find the canvas
-    const sprite_canvas = document.getElementById("painting-grid");
-
-    // Loop through 40x30 times
-    for (
-      let pixel_number = 0;
-      pixel_number < sprite_canvas.children.length;
-      pixel_number++
-    ) {
-      const pixel = sprite_canvas.children[pixel_number];
-
-      const r = raw_data[pixel_number * 4 + 0];
-      const g = raw_data[pixel_number * 4 + 1];
-      const b = raw_data[pixel_number * 4 + 2];
-      const a = raw_data[pixel_number * 4 + 3];
-
-      pixel.style.backgroundColor = `rgba(${r},${g},${b},${a / 255})`;
-    }
+    ctx.drawImage(img, 0, 0);
   };
-}
-
-// Function to create the pixels in the canvas
-function add_pixels_to_canvas() {
-  // Find the canvas
-  const sprite_canvas = document.getElementById("painting-grid");
-
-  // Loop through 40x30 times
-  for (let x = 0; x < 40; x++) {
-    for (let y = 0; y < 30; y++) {
-      // Make a new pixel
-      const pixel = document.createElement("div");
-      // Edit it
-      pixel.className = "pixel";
-      // Make a new color box
-      const color_box = document.createElement("div");
-      // Edit it
-      color_box.className = "color-box";
-      // Add it as a child to the pixel
-      pixel.appendChild(color_box);
-      // Add it as a child to the canvas
-      sprite_canvas.appendChild(pixel);
-    }
-  }
 }
 
 // Function to load the sprites into the holder
